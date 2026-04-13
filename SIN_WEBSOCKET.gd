13aa6046835d806587e18be8264c3bb5ea439d82
Delalex <delalexdev@ya.ru>
2026-04-13 23:05:30 +0300

    Документация + Лицензия

--- SIN_WEBSOCKET.gd
+++ SIN_WEBSOCKET.gd
@@ -1,6 +1,7 @@
 extends Node
 
 # Сигналы для подключения
+# Signals for connection
 signal client_connected()
 signal client_disconnected()
 signal data_received_string(data: String)
@@ -12,31 +13,9 @@ var clients: Array = []
 var is_running: bool = false
 
 func _ready():
-	start_server()
+	pass
 
-func start_server() -> bool:
-	var err = tcp_server.listen(PORT, "0.0.0.0")
-	if err == OK:
-		is_running = true
-		print("WEBSOCKET >>> ESTABLISHED ", get_local_ip(), ":", PORT)
-		return true
-	else:
-		push_error("WEBSOCKET >>> ERROR: ", err)
-		return false
-
-func stop_server():
-	tcp_server.stop()
-	is_running = false
-	for ws in clients:
-		ws.close()
-	clients.clear()
-
-func get_local_ip() -> String:
-	for ip in IP.get_local_addresses():
-		if ip.begins_with("192.168.") or ip.begins_with("10.0.") or ip.begins_with("172.16."):
-			return ip
-	return "127.0.0.1"
-
+#region UNNECESSARY LOOP ROUTINE
 func _process(_delta):
 	if not is_running or not tcp_server.is_listening():
 		return
@@ -78,23 +57,57 @@ func _process_packets(ws: WebSocketPeer):
 			emit_signal("data_received_string", msg)
 		else:
 			emit_signal("data_received_bytes", packet)
+#endregion
 
-# Отправка данных
+# Запуск Вебсокет сервера
+# Start Websocket server
+func start_server() -> bool:
+	var err = tcp_server.listen(PORT, "0.0.0.0")
+	if err == OK:
+		is_running = true
+		print("WEBSOCKET >>> ESTABLISHED ", get_local_ip(), ":", PORT)
+		return true
+	else:
+		push_error("WEBSOCKET >>> ERROR: ", err)
+		return false
+
+# Остановка Вебсокет сервера
+# Stop Websocket server
+func stop_server():
+	tcp_server.stop()
+	is_running = false
+	for ws in clients:
+		ws.close()
+	clients.clear()
+
+# Функция получения локального IP для подключения
+# Get Local IP address to connect clients
+func get_local_ip() -> String:
+	for ip in IP.get_local_addresses():
+		if ip.begins_with("192.168.") or ip.begins_with("10.0.") or ip.begins_with("172.16."):
+			return ip
+	return "127.0.0.1"
+
+# Функция отправки текстовых данных
+# Send string function
 func send_text(message: String):
 	for ws in clients:
 		if ws.get_ready_state() == WebSocketPeer.STATE_OPEN:
 			ws.send_text(message)
 
+# Функция для отправки байтовых данных
+# Send bytes function
 func send_bytes(data: PackedByteArray):
 	for ws in clients:
 		if ws.get_ready_state() == WebSocketPeer.STATE_OPEN:
 			ws.put_packet(data)
 
-func broadcast(message: String):
-	send_text(message)
-
+# Получить кол-во подключенных клиентов
+# Get count of connected clients
 func get_client_count() -> int:
 	return clients.size()
 
+# Есть ли вообще клиенты у сервера?
+# Do server has connected clients?
 func has_clients() -> bool:
 	return not clients.is_empty()
